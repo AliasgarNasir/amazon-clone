9a6636e814e16d5695b6c213dddf8b1acd9acbaf
Aliasgar nasir <nasiraliasgar@gmail.com>
2021-03-10 00:15:31 +0530

    Amazon NavBar Added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBqCi-MeN-bAn3GPlx0BfnAy2fYMC9V68Q",
+    authDomain: "clone-78e73.firebaseapp.com",
+    projectId: "clone-78e73",
+    storageBucket: "clone-78e73.appspot.com",
+    messagingSenderId: "382070174965",
+    appId: "1:382070174965:web:5a5fe0258b7c00e578c1bc",
+    measurementId: "G-H2TFG0R6Z1"
+  };
